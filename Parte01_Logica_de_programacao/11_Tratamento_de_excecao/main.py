e6e0a69ae016d97816f06d618f6aa00105568004
Valter-J <valterjpereiras@gmail.com>
2026-07-02 20:08:11 -0300

    Tratamento de exececao

---
+++ Parte01_Logica_de_programacao/11_Tratamento_de_excecao/main.py
@@ -0,0 +1,10 @@
+# Tratamento de exceção
+
+try:
+    #TODO: SEU CODIGO FICA AQUI
+    n = int(input("Informe um número inteiro: "))
+    # Saída de dados
+    print((f"Número informado: {n}. "))
+
+except:
+    print("Código não pode ser executado. ")
---
+++ Parte01_Logica_de_programacao/12_loop/main.py
@@ -0,0 +1,13 @@
+# Tratamento de execeção 
+try:
+    # TODO: 
+
+    n = int(input("Informe um número inteiro: "))
+
+    # laço de repetição
+
+    while n >= 0:
+        print(n)
+    pass
+except:
+    print("Não foi possível exibir a contagem. ")
