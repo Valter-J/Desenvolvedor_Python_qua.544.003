358729170beb9bf7d35be9be32cc0e6e31254160
Valter-J <valterjpereiras@gmail.com>
2026-07-28 23:05:08 -0300

    Atividade 2, com função

--- Atividades/lista_atividades_01/Atividades_01_02/main.py
+++ Atividades/lista_atividades_01/Atividades_01_02/main.py
@@ -15,63 +15,99 @@ caso o usuário tenha a idade mínima, o programa grava em arquivo
 
 import os
 
+def limpar():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def menu():
+    print("\n=========================================\n")
+    print("Bem vindo ao Cinema da Bagunça")
+    print("Por favor escolha uma das opções abaixo: ")
+    print("0 - A volta dos que não foram. CLASS: livre ")
+    print("1 - A roda Quadrada. CLASS 12 Anos")
+    print("2 - As tranças do Rei Careca. CLASS: 14 Anos")
+    print("3 - Poeira em Alto Mar. CLASS: 16 Anos")
+    print("4 - A vingança do Frango Assado. CLASS: 18 anos")
+
+    filmes = [["A volta dos que não foram. ", 0],
+             ["A roda Quadrada. ", 12],
+             ["As tranças do Rei Careca.", 14],
+             ["Poeira em Alto Mar.", 16],
+             ["A Vingaça do Frango Assado",18]]
+
+
+    indice = filmes[int(input(""))][1]
+    
+    return indice
+
+    
+def pegarFilme():
+
+    indice = menu()
+
+    filmes = [["A volta dos que não foram. ", 0],
+                 ["A roda Quadrada. ", 12],
+                 ["As tranças do Rei Careca.", 14],
+                 ["Poeira em Alto Mar.", 16],
+                 ["A Vingaça do Frango Assado",18]]
+
+    for i in filmes:
+        i
+        if(indice == i[1]):
+            valor = i[0]
+            break
+
+    return valor
+
+
+def gerarBilhete(nome, idade):
+
+    valorFilme= pegarFilme()
+
+    with open(f"Atividades_01_02/arquivos/{nome}.txt", "w", encoding="utf-8") as arquivo:
+        arquivo.write(
+            f"\n=======================================\n"
+            f"\n======BEM VINDO AO CINEMINHA ==========\n"
+            f"\n=============={nome}===================="
+            f"\n=============={idade}==================="
+            f"\n======{valorFilme}======"
+            "======================================"
+        )
+    
+def abrirbilhete(nome):
+
+    with open(f"Atividades_01_02/arquivos/{nome}.txt", "r", encoding="utf-8") as arquivo:
+        conteudo = arquivo.read()
+
+        print(conteudo)
+
+
+def verificacaoIdade(idade):
+    valorMenu = menu()
+    if (idade >=valorMenu):
+        return True
+    else:
+        return False
+
+    
 try:
+
     nome =input("Por favor digite seu Nome:").strip()
     idade = int(input("Por favor digite sua idade"))
     
     while True:
-        
-        print("\n=========================================\n")
-        print("Bem vindo ao Cinema da Bagunça")
-        print("Por favor escolha uma das opções abaixo: ")
-        print("1 - A volta dos que não foram. CLASS: livre ")
-        print("2 - A roda Quadrada. CLASS 12 Anos")
-        print("3 - As tranças do Rei Careca. CLASS: 14 Anos")
-        print("4 - Poeira em Alto Mar. CLASS: 16 Anos")
-        print("5 - A vingança do Frango Assado. CLASS: 18 anos")
-        opcao = int(input())
-        
-        os.system("cls" if os.name == "nt" else "clear")
-        
-        match opcao:
-            
-            case 1:
-                if (idade > 0):
-                    print("Bem vindo a sala 1, Bom filme. ")
-                    break
-                    
-            case 2: 
-                if (idade > 12):
-                    print("Bem vindo a sala 2, Bom filme. ")
-                    break
-                else:
-                    print("Opss, você não tem idade Suficiente. Por favor retorne ao MENU. ")
-                    continue
-            case 3:
-                if (idade > 14):
-                    print("Bem vindo a sala 3, Bom filme. ")
-                    break
-                else:
-                    print("Opss, você não tem idade Suficiente. Por favor retorne ao MENU. ")
-                    continue
-            case 4:
-                if (idade > 14):
-                    print("Bem vindo a sala 4, Bom filme. ")
-                    break
-                else:
-                    print("Opss, você não tem idade Suficiente. Por favor retorne ao MENU. ")
-                    continue
-            case 5:
-                if (idade > 14):
-                    print("Bem vindo a sala 5, Bom filme. ")
-                    break
-                else:
-                    print("Opss, você não tem idade Suficiente. Por favor retorne ao MENU. ")
-                    continue
-            case _:
-                print("Opção inválida, Por favor digite um número do MENU. ")
-                continue
-    
+
+        processo = True
+        menu()
+        pegarFilme()
+        limpar()
+        if verificacaoIdade(idade) == True:
+            gerarBilhete(nome, idade)
+            abrirbilhete(nome)
+            processo = False
+            break
+        else:
+            print("Opção inválida, Por favor digite um número do MENU. ")
+            continue    
     
 except Exception as e:
     print(f"Opção inválida {e}") 
