05b09b3480012c21e7113affaeeed1f8ade7bb94
Valter-J <valterjpereiras@gmail.com>
2026-08-03 21:33:55 -0300

    listas

---
+++ Parte02_Colecoes/01_lista/main.py
@@ -0,0 +1,14 @@
+# lista de nomes
+
+nomes = ["Alex", "Jesus", "Luciano", "Raquel", "Rose"]
+
+# Exibe o primeiro elemento da lista
+
+print(nomes[0])
+print(nomes[4])
+print(nomes)
+
+
+for i in nomes:
+    print(i)
+
---
+++ Parte02_Colecoes/02_lista_numerada/main.py
@@ -0,0 +1,5 @@
+frutas = ["Maça", "Abacaxi", "Morango", "Maracujá"]
+
+
+for i,fruta in enumerate(frutas, start=1):
+    print(f"Fruta {i}: {fruta}")
