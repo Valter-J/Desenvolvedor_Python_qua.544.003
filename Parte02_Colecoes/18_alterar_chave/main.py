7cd9999aa505770cd0d2d8deb3f5e695a30b2ccb
Valter-J <valterjpereiras@gmail.com>
2026-08-05 21:32:18 -0300

    alterar chave

---
+++ Parte02_Colecoes/18_alterar_chave/main.py
@@ -0,0 +1,13 @@
+usuario = {
+    'nome': "Fulano de Tal",
+    'idade': 35,
+    'email': "fulano@gmail.com",
+    'Cpf': "123.456.789-12"
+}
+
+# usuário informa a chave que deseja alterar
+
+chave = input("informe o nome da chave: ").strip().lower()
+
+
+# TODO: VERIFICA SE A CHAVE EXISTE
